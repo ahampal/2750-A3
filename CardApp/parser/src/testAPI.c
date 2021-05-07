ec3d76ef748f0e25687a89ec99938dff994ebbc8
ahampal <ahampal@uoguelph.ca>
2021-05-06 23:03:24 -0400

    house keeping

--- CardApp/parser/src/testAPI.c
+++
@@ -1,9 +0,0 @@
-#include "VCardParser.h"
-
-int main(int argc, char **argv) {
-    char *retString;
-    retString = CardToJSON(argv[1]);
-    printf("%s\n", retString);
-    free(retString);
-    return 0;
-}
